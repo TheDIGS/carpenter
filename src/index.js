137a3f98436ef69fda0fdc97ea5e9bace3832e9a
digs <thedigs06@gmail.com>
2020-11-13 21:17:34 +0000

    things

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const {KlasaClient} = require('klasa');
+
+const client = new KlasaClient(require('./config'));
+
+KlasaClient.defaultGuildSchema.add('roles', schema => schema.add('muted', 'role'));
+KlasaClient.defaultUserSchema
+	.add('experience', 'Integer', {
+		default: 0,
+		configurable: false
+	})
+	.add('level', 'Integer', {
+		default: 0,
+		configurable: false
+	});
+
+console.log(process.env)
+
+client.login(process.env.token);
